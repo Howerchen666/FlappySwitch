de41c501dfc604806da03698864d24439a501003
Tom Shen <tomshen2004@163.com>
2021-06-11 13:38:03 +0800

    Initial Commit

---
+++ SwitchGames/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwitchGames
+//
+//  Created by Tom Shen on 2021/6/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwitchGames/SwitchGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchGamesApp.swift
+//  SwitchGames
+//
+//  Created by Tom Shen on 2021/6/11.
+//
+
+import SwiftUI
+
+@main
+struct SwitchGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
