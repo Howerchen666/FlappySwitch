c5eddf66c631692ca9dc2a0cf38e6abb9210342f
Tom Shen <tomshen2004@163.com>
2021-06-11 13:49:02 +0800

    Crafted Switch test

--- SwitchGames/ContentView.swift
+++ SwitchGames/ContentView.swift
@@ -7,15 +7,39 @@
 
 import SwiftUI
 
+// Size 9 X 9
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Toggle("1", isOn: .constant(true))
+            Toggle("1", isOn: .constant(true))
+            Toggle("1", isOn: .constant(true))
+            Toggle("1", isOn: .constant(true))
+            Toggle("1", isOn: .constant(true))
+            Toggle("1", isOn: .constant(true))
+            Toggle("1", isOn: .constant(true))
+            Toggle("1", isOn: .constant(true))
+            Toggle("1", isOn: .constant(true))
+        }
+        .labelsHidden()
+        .overlay(OverlayControlView())
+    }
+}
+
+struct OverlayControlView: View {
+    var body: some View {
+        Color.white
+            .opacity(0.5)
+            .onTapGesture {
+                print("Tapped")
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
